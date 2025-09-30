aa4c8e9cab55c69ebc7f36d649dae02bfc0ec9eb
johnzerv <jzervakis@hotmail.com>
2025-09-30 15:03:39 +0300

    evaluation of blendsql join for Q12

--- evaluation/join/Q12/eval_runs.sh
+++ evaluation/join/Q12/eval_runs.sh
@@ -6,5 +6,6 @@ for size in "${sizes[@]}"; do
     for model in "${models[@]}"; do
         echo "Evaluating with -s $size and -m $model"
         python evaluation/join/Q12/eval_scripts/lotus_q12_eval.py -s $size -m $model -p vllm
+        python evaluation/join/Q12/eval_scripts/blendsql_q12_eval.py -s $size -m $model -p vllm
     done
 done
--- evaluation/join/Q12/eval_scripts/blendsql_q12_eval.py
+++ evaluation/join/Q12/eval_scripts/blendsql_q12_eval.py
@@ -11,7 +11,11 @@ df_movies = pd.read_csv("datasets/movies_directors/movies.csv").head(args.size)
 df_directors = pd.read_csv("datasets/movies_directors/directors.csv")
 df_movies_directors = df_movies.merge(df_directors, left_on=['director_id'], right_on=['id'])[['title', 'director_name']]
 
-results_file = f"evaluation/join/Q12/results/blendsql_Q12_join_{args.model.replace(':', '_')}_{args.provider}_{args.size}.csv"
+if args.provider == 'ollama':
+    results_file = f"evaluation/join/Q12/results/blendsql_Q12_join_{args.model.replace(':', '_')}_{args.provider}_{args.size}.csv"
+elif args.provider == 'vllm':
+    results_file = f"evaluation/join/Q12/results/blendsql_Q12_join_{args.model.replace('/', ':')}_{args.provider}_{args.size}.csv"
+
 blendsql_res = pd.read_csv(results_file)[['title', 'director_name']]
 
 df = df_movies_directors.merge(blendsql_res, on=['title', 'director_name'], how='outer', indicator=True)
