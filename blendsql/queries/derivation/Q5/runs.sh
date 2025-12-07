78325e03570e097323ae25497d121ba8d5652b4b
Yannis Zervakis <jzervakis@hotmail.com>
2025-12-07 12:23:15 +0000

    blendsql Q5

--- blendsql/queries/derivation/Q5/runs.sh
+++ blendsql/queries/derivation/Q5/runs.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
-# Runs for Q5
+sizes=(10 20 50)
+models_ollama=("gemma3:12b" "llama3.3:70b")
+models_vllm=("meta-llama/Llama-3.1-8B-Instruct")
+# models_dev=("RedHatAI/Llama-3.3-70B-Instruct-quantized.w8a8")
+models_dev=("gemma3:12b")
+sizes_dev=(10)
+
+dev="${1:-}"
+
+
+if [ "$dev" != "dev" ]; then
+    for size in "${sizes[@]}"; do
+        for model in "${models_ollama[@]}"; do
+            echo "Running Q5 with -s $size and m $model"
+            python blendsql/queries/derivation/Q1/q1.py --wandb -s $size -m $model -p ollama
+        done
+    done
+
+    for size in "${sizes[@]}"; do
+        for model in "${models_vllm[@]}"; do
+            echo "Running Q5 with -s $size and m $model"
+            python blendsql/queries/derivation/Q1/q1.py --wandb -s $size -m $model -p vllm
+        done
+    done
+else
+    for size in "${sizes_dev[@]}"; do
+        for model in "${models_dev[@]}"; do
+            echo "Running Q5 with -s $size and m $model"
+            python blendsql/queries/derivation/Q5/q5.py  -s $size -m $model -p ollama
+            echo "Evaluating Q5 with -s $size and m $model executed from blendsql"
+            python evaluation/derivation/Q5/eval_scripts/blendsql_q5_eval.py  -s $size -m $model -p ollama
+        done
+    done
+fi
