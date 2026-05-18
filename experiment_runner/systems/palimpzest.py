039af4fb38d1efa9560fc888f9323ffd99e19461
Yannis Zervakis <jzervakis@hotmail.com>
2026-05-18 09:04:14 +0000

    selection queries for palimpzest

--- experiment_runner/systems/palimpzest.py
+++ experiment_runner/systems/palimpzest.py
@@ -6,7 +6,6 @@ import pandas as pd
 from dotenv import load_dotenv
 from ollama import chat
 
-
 load_dotenv()
 
 class PalimpzestSystem(BaseSystem):
@@ -64,6 +63,45 @@ class PalimpzestSystem(BaseSystem):
         output = output.run(config=config)
 
         print(output.execution_stats)
+        
+        execution_time = output.execution_stats.total_execution_time
+        input_tokens=output.execution_stats.total_input_tokens
+        output_tokens=output.execution_stats.total_output_tokens
+        total_tokens=output.execution_stats.total_tokens
+        total_calls=input_size
+        tokens_throughput = total_tokens / execution_time if execution_time > 0 else 0
+
+        return {"result": output.to_df(), 
+                "latency": execution_time,
+                "input_tokens": input_tokens, 
+                "output_tokens": output_tokens, 
+                "total_tokens": total_tokens, 
+                "total_calls": total_calls,
+                "tokens_throughput": tokens_throughput
+                }
+
+
+    def execute_selection_query(
+        self,
+        nl_criterion: str,
+        input_size: int,
+        table: str = None,
+        cols: list = None,
+        **kwargs
+    ) -> dict:
+        input_df = pd.read_csv(f"../{table}")[cols].head(input_size)
+        summaries = pz.MemoryDataset(id=table.split('/')[-1].split('.')[0], vals=input_df)
+
+        output = summaries.sem_filter(
+            nl_criterion,
+            depends_on=cols,
+        )
+
+        config = pz.QueryProcessorConfig(
+            available_models=[self.model],
+            timeout=50000,
+        )
+        output = output.run(config=config)
         
         execution_time = output.execution_stats.total_execution_time
         input_tokens=output.execution_stats.total_input_tokens
